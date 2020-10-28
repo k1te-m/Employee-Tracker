6bda625dedfd6e360147036b558787f495e60960
k1te <kmiller343@gmail.com>
2020-10-28 00:47:58 -0500

    wrote schema for department & role tables

---
+++ schema.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE widgetcompany_db;
+
+USE widgetcompany_db;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY department_id REFERENCES department(id)
+);
