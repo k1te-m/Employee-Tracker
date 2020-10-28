b50b5ad86d2efe197030fa86da25e637ca8b3f78
k1te <kmiller343@gmail.com>
2020-10-28 02:29:36 -0500

    wrote basic init() addEntity() finished addDep()

--- widgetFunctions.js
+++ widgetFunctions.js
@@ -1,13 +1,28 @@
 // Dependencies
 const inquirer = require("inquirer");
+const mysql = require("mysql");
+
+
+// Create Connection
+const connection = mysql.createConnection({
+    host: "localhost",
+  
+    port: 3306,
+  
+    user: "root",
+  
+    password: "",
+    database: "widgetcompany_db",
+  });
+
 
 // Inquirer Prompt Questions
 
-const initQuestions = [
+const initQs = [
   {
     type: "list",
     choices: [
-      "Add Departments/Roles/Employees",
+      "Add Department/Role/Employee",
       "View Departments/Roles/Employees",
       "Update Employee Roles",
       "EXIT",
@@ -18,17 +33,96 @@ const initQuestions = [
   },
 ];
 
+const addDREQs = [
+  {
+    type: "list",
+    choices: ["Department", "Role", "Employee"],
+    name: "addWhat",
+    message: "What type of entity would you like to add?",
+  },
+];
+
+const addDQs = [
+{
+    type: "input",
+    name: "departmentName",
+    message: "What is the new department name?"
+}
+]
+
+// const addRQs = [
+// {
+//     type: "input",
+//     name: "roleTitle",
+//     message: "What is the role title?"
+// },
+// {
+//     type: "number",
+//     name: "roleSalary",
+//     message: "What is the role salary?"
+// },
+// {
+//     type: "list",
+//     choices:
+// }
+// ]
 
 // Functions
 
-function init() {
-    inquirer.prompt(initQuestions).then(userAction => {
-        const { action } = userAction;
-        console.log(action);
-        // switch (action) {
-        //     case "Add Departments/Roles/Employees"
-        // }
+function addDepartment() {
+    inquirer.prompt(addDQs).then(userDep => {
+        const { departmentName } = userDep;
+        connection.query(
+            `INSERT INTO department SET name = "${departmentName}"`,
+            function (err, res) {
+                if (err) throw err;
+                console.log(`${res.affectedRows} department added.\n Department: ${departmentName}`)
+            }
+        )
+    })
+};
+
+function addRole() {
+
+};
+
+function addEmployee() {
+
+};
+
+function addEntity() {
+    inquirer.prompt(addDREQs).then(userAdd => {
+        const { addWhat } = userAdd;
+        switch (addWhat) {
+            case "Department":
+                addDepartment();
+                break;
+            case "Role":
+                break;
+            case "Employee":
+                break;
+        }
+    
     })
 }
 
-module.exports = init;
+function init() {
+    inquirer.prompt(initQs).then(userAction => {
+        const { action } = userAction;
+        console.log(action);
+        switch (action) {
+            case "Add Department/Role/Employee":
+                addEntity();
+                break;
+            case "View Departments/Roles/Employees":
+                break;
+            case "Update Employee Roles":
+                break;
+            case "EXIT":
+                connection.end();
+                break;
+        }
+    })
+}
+
+module.exports = init, addEntity, addDepartment;
