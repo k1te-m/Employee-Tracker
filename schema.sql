66f3012f0d19cdb856dd0dcf8a326046d3c0a9a8
k1te <kmiller343@gmail.com>
2020-10-28 00:52:37 -0500

    wrote schema fore employee table

--- schema.sql
+++ schema.sql
@@ -16,3 +16,13 @@ CREATE TABLE role (
     PRIMARY KEY (id),
     FOREIGN KEY department_id REFERENCES department(id)
 );
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT REFERENCES employee(id),
+    PRIMARY KEY (id),
+    FOREIGN KEY role_id REFERENCES role(id)
+);
